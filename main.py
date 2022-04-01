69233b6bd15ad80f198c0f46bb6f60febbb3aab1
YourDad07 <salmanadhikari415@gmail.com>
2022-04-01 18:32:20 +0530

    Change The Title

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from models.db import DB
 from public.validation import Validate_Data
 ws = Tk()
 ws.geometry("940x500")
+ws.title("120A3043")
 Vd = Validate_Data(ws)
 db = DB(ws)
 frame_1 = Home(ws)
